c4bd0efcd25ea2298e0a5b4c43a91df5ce22f0f3
Ortwin Joniaux <ortwin@mobilityplus.be>
2021-10-17 20:56:44 +0200

    Fixed logging and added shell scripts for convenience.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module hellofx {
     requires javafx.graphics;
     requires lombok;
     requires org.slf4j;
+    requires org.slf4j.simple;
     requires opencv;
 
     opens ortwin to javafx.fxml;
